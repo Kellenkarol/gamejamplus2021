b80e3a91b9cfa0a976eb35d737927be0b88bddcf
edilsonchaves <edilson.montenegro@gmail.com>
2021-10-10 12:51:59 -0300

    Initial Menu Completed

--- Game Jam Plus 2021 Projeto/Assets/Script/ToggleScriptMusic.cs
+++ Game Jam Plus 2021 Projeto/Assets/Script/ToggleScriptMusic.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
+using Utils;
 public class ToggleScriptMusic : MonoBehaviour, IPointerDownHandler
 {
     //Detect current clicks on the GameObject (the one with the script attached)
     [SerializeField] bool statusToggle;
     [SerializeField] AudioSource audioToggle;
+    [SerializeField] Image image;
+    [SerializeField] Array<Sprite> sprites;
     private void Start()
     {
         statusToggle = true;
@@ -17,6 +20,10 @@ public class ToggleScriptMusic : MonoBehaviour, IPointerDownHandler
         //Output the name of the GameObject that is being clicked
         statusToggle = !statusToggle;
         AudioManager.Instance.ChangeMusicStatus(statusToggle);
+        if (statusToggle)
+            image.sprite = sprites.GetElement(0);
+        else
+            image.sprite = sprites.GetElement(1);
         audioToggle.Play();
     }
 }
